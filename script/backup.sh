d452b2fff5d06af2a49c3a185df6b9a232067ea6
ShroXd <shroxdf@gmail.com>
2023-08-13 22:19:20 +0800

    fix: fix the logic and run a backup script

--- script/backup.sh
+++ script/backup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-CURRENT_DATE=$(date + "%Y-%m-%d")
-BACKUP_FOLDER="$(pwd)/../config"
+CURRENT_DATE=$(date "+%Y-%m-%d")
+REPO_DIR="$(git rev-parse --show-toplevel)"
 
-BACKUP_DIR="$BACKUP_FOLDER/$CURRENT_DATE"
+# Start backup config files
+
+CONFIG_BACKUP_DIR="$REPO_DIR/config/$CURRENT_DATE"
 
 # Create a backup folder with the current date as its name
-mkdir -p "$BACKUP_DIR"
+mkdir -p "$CONFIG_BACKUP_DIR"
 
 # Backup system environment file
-cp /etc/environment "$BACKUP_DIR"
-echo "Config file /etc/environment backed up to $BACKUP_DIR"
+cp /etc/environment "$CONFIG_BACKUP_DIR"
+echo "Config file /etc/environment backed up to $CONFIG_BACKUP_DIR"
