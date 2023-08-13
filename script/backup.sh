142d852f3418f6a8be9de8231dede4149660b459
ShroXd <shroxdf@gmail.com>
2023-08-13 22:23:24 +0800

    refactor: it's hard to use date folder for storing nvim config

--- script/backup.sh
+++ script/backup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-CURRENT_DATE=$(date "+%Y-%m-%d")
+#CURRENT_DATE=$(date "+%Y-%m-%d")
 REPO_DIR="$(git rev-parse --show-toplevel)"
 
 # Start backup config files
 
-CONFIG_BACKUP_DIR="$REPO_DIR/config/$CURRENT_DATE"
+#CONFIG_BACKUP_DIR="$REPO_DIR/config/$CURRENT_DATE"
+CONFIG_BACKUP_DIR="$REPO_DIR/config"
 
 # Create a backup folder with the current date as its name
 mkdir -p "$CONFIG_BACKUP_DIR"
