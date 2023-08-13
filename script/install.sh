5d85485b17087e461a0c5c749a628e2e72c7df94
ShroXd <shroxdf@gmail.com>
2023-08-13 22:50:40 +0800

    refactor: backup existing nvim config folder

--- script/install.sh
+++ script/install.sh
@@ -3,5 +3,14 @@
 REPO_DIR="$(git rev-parse --show-toplevel)"
 
 # Sync lazy nvim config
-ln -s "$REPO_DIR/config/nvim" "$HOME/.config/nvim"
+NVIM_LINK_NAME="$HOME/.config/nvim"
 
+# Check if ~/.config/nvim exists
+if [ -d "$NVIM_LINK_NAME" ]; then
+	# If the directory exists, back it up as nvim-backup
+	BACKUP_NAME="$HOME/.config/nvim-backup"
+	echo "Backing up existing nvim directory to $BACKUP_NAME"
+	mv "$NVIM_LINK_NAME" "$BACKUP_NAME"
+fi
+
+ln -s "$REPO_DIR/config/nvim" "$NVIM_LINK_NAME"
