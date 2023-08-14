5ce7291e036f46447a3461738b5d62a9a202a5f9
ShroXd <shroxdf@gmail.com>
2023-08-14 12:30:20 +0800

    feat: mange config for bspwm

--- script/install.sh
+++ script/install.sh
@@ -19,6 +19,21 @@ ln -s "$REPO_DIR/config/nvim" "$NVIM_LINK_NAME"
 
 # --------------------------------------------------------------
 
+# Sync lazy bspwm config
+BSPWM_LINK_NAME="$HOME/.config/bspwm"
+
+# Check if ~/.config/nvim exists
+if [ -d "$BSPWM_LINK_NAME" ]; then
+	# If the directory exists, back it up as nvim-backup
+	BACKUP_NAME="$HOME/.config/bspwm-backup"
+	echo "Backing up existing bspwm directory to $BACKUP_NAME"
+	mv "$BSPWM_LINK_NAME" "$BACKUP_NAME"
+fi
+
+ln -s "$REPO_DIR/config/bspwm" "$BSPWM_LINK_NAME"
+
+# --------------------------------------------------------------
+
 # Sync .zshrc
 
 ZSH_LINK_NAME="$HOME/.zshrc"
